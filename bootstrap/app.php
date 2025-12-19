d9a3d57c742d3481c1a8c5c1d5de8abdfc45e1e4
hanrsyidin <142026978+hanrsyidin@users.noreply.github.com>
2025-12-19 18:11:58 +0700

    refactor: fix redirect after login admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->trustProxies(at: '*');
+        $middleware->redirectUsersTo('/admin/beranda');
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
