8878475531cc466b4d7e32472405113a1772a294
hanrsyidin <142026978+hanrsyidin@users.noreply.github.com>
2026-01-04 20:59:07 +0700

    feat: create struktur_pimpinan table

--- database/migrations/2025_11_03_070636_create_struktur_pimpinan_table.php
+++ database/migrations/2025_11_03_070636_create_struktur_pimpinan_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('struktur_pimpinans', function (Blueprint $table) {
+        Schema::create('struktur_pimpinan', function (Blueprint $table) {
             $table->id();
             $table->string('content');
             $table->timestamps();
@@ -22,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('struktur_pimpinans');
+        Schema::dropIfExists('struktur_pimpinan');
     }
 };
